96bc23d7ec3e5d8d6c499df18aea38e4c76936e5
Adam Walker <adamw207@yahoo.com>
2021-04-15 19:00:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_04_15_182226) do
 
+
+
+  create_table "movies", force: :cascade do |t|
+    t.string  "title"
+    t.integer "release_date"
+    t.string  "director"
+    t.string  "lead"
+    t.boolean "in_theaters"
+  end
+
 end
